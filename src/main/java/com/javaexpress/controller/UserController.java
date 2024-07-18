b4a99074a3939ff55eeceb11a28c2a4d678448e2
javaexpresschannel <javaexpresschannel@gmail.com>
2024-07-18 14:43:51 +0530

    fake commit

--- src/main/java/com/javaexpress/controller/UserController.java
+++ src/main/java/com/javaexpress/controller/UserController.java
@@ -23,7 +23,7 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
-	@PostMapping(value="/create1",produces = {"application/json"},consumes = {"application/json"})
+	@PostMapping(value="/createuser",produces = {"application/json"},consumes = {"application/json"})
 	public User createUser(@RequestBody User user) {
 		log.info("UserController :: createUser {} ",user);
 		return userService.createUser(user);
